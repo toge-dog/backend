871e53a5328d33ec0146e7f01c5db25c49c708c7
gwanghui97 <lgh970409@gmail.com>
2024-08-13 21:04:12 +0900

    Init Backend

---
+++ src/main/java/com/togedog/TogeDogApplication.java
@@ -0,0 +1,11 @@
+package com.togedog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogeDogApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TogeDogApplication.class, args);
+  }
+}
