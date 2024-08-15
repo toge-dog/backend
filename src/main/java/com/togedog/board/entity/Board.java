db7590eba268ad908494de6b97b79655cfefc60f
GeeksforGeeks <scientist5@naver.com>
2024-08-15 21:37:25 +0900

    feat:#12 Super Table Board Entity,Dto 구현

--- src/main/java/com/togedog/board/entity/Board.java
+++ src/main/java/com/togedog/board/entity/Board.java
@@ -38,8 +38,8 @@ public class Board extends Auditable {
     public enum BoardType {
         REPORT("신고 게시판"),
         INQUIRY("문의 게시판"),
-        BOAST("자랑 게시판"),
         REVIEW("후기 게시판"),
+        BOAST("자랑 게시판"),
         NOTICE("공지 게시판");
 
         @Getter
