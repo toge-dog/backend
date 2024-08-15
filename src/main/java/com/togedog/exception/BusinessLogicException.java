d4686cd23669b5c76eaab4ea778946c573a07243
Tizesin <tlsalswns0o@gmail.com>
2024-08-15 20:26:55 +0900

    fix:#39 excpetion 버그 수정

--- src/main/java/com/togedog/exception/BusinessLogicException.java
+++ src/main/java/com/togedog/exception/BusinessLogicException.java
@@ -7,7 +7,7 @@ public class BusinessLogicException extends RuntimeException {
     private ExceptionCode exceptionCode;
 
     public BusinessLogicException(ExceptionCode exceptionCode) {
-        super(exceptionCode.getMsg());
+        super(exceptionCode.getDescription());
         this.exceptionCode = exceptionCode;
     }
 }
