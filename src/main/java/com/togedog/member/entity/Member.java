d08c097841561ced3eb443a50fa48e36f8c2d7d1
JinHyeok Hwang <dujin132@gmail.com>
2024-08-18 18:17:05 +0900

    충동해결

--- src/main/java/com/togedog/member/entity/Member.java
+++ src/main/java/com/togedog/member/entity/Member.java
@@ -1,7 +1,5 @@
 package com.togedog.member.entity;
 
-import com.togedog.friend.entity.Friend;
-import com.togedog.matching.entity.Matching;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity(name = "member")
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
@@ -38,9 +36,6 @@ public class Member {
     @Column(name = "member_phone", nullable = false)
     private String phone;
 
-    @Column(name = "address", nullable = false)
-    private String address;
-
     @Column(name = "member_profile_image")
     private String profileImage;
 
@@ -52,41 +47,14 @@ public class Member {
     private int reportCount;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "member_status", nullable = false)
+    @Column(name = "member_report_status", nullable = false)
     private memberStatus status = Member.memberStatus.RESTRICTION;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    @OneToMany(mappedBy = "member")
-    private List<Friend> friends = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
-    private List<Friend> members = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member")
-    private List<Matching> matchings = new ArrayList<>();
-
-    public void addMatching(Matching matching) {
-        this.matchings.add(matching);
-        if(matching.getMember() != this) {
-            matching.setMember(this);
-        }
-    }
-
-    public void addFriend(Friend friend) {
-        this.friends.add(friend);
-        if (friend.getMember() != this) {
-            friend.setMember(this);
-        }
-    }
-
-    public void addMember(Friend friend) {
-        this.members.add(friend);
-        if (friend.getMember() != this) {
-            friend.setMember(this);
-        }
-    }
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
 
     public enum memberGender {
         M("남성"),
