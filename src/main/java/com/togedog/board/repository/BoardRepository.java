ac56894db63b00db3f9b6c3277dc9952d3bed934
GeeksforGeeks <scientist5@naver.com>
2024-08-18 15:49:52 +0900

    feat:#8 Super Table board repository 구현

---
+++ src/main/java/com/togedog/board/repository/BoardRepository.java
@@ -0,0 +1,13 @@
+package com.togedog.board.repository;
+
+import com.togedog.board.entity.Board;
+import com.togedog.member.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByMember (Member member);
+}
