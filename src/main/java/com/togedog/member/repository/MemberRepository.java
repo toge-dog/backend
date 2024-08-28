26af58b7bb9ca98e049cd520cecb2fe28c31dc4e
gwanghui97 <lgh970409@gmail.com>
2024-08-28 11:44:53 +0900

    feat:#7 @Repository 추가

--- src/main/java/com/togedog/member/repository/MemberRepository.java
+++ src/main/java/com/togedog/member/repository/MemberRepository.java
@@ -4,10 +4,12 @@ import com.togedog.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByPhone(String phone);
