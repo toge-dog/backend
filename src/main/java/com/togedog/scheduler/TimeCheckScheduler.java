996c69cc1fdccca3d3666ea1ba9e003a295c408b
신민준 <161104180+Tizesin@users.noreply.github.com>
2025-02-20 17:13:08 +0900

    Update TimeCheckScheduler.java

--- src/main/java/com/togedog/scheduler/TimeCheckScheduler.java
+++ src/main/java/com/togedog/scheduler/TimeCheckScheduler.java
@@ -1,7 +1,5 @@
 package com.togedog.scheduler;
 
-import com.togedog.matchingStandBy.entity.MatchingStandBy;
-import com.togedog.matchingStandBy.repository.MatchingStandByRepository;
 import com.togedog.matchingStandBy.service.MatchingStandByService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
@@ -9,23 +7,20 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class TimeCheckScheduler {
     private final MatchingStandByService service;
 
-    // 비동기 스케줄러: 1분마다 현재 시간 출력
+    // 매일 00:00 (자정)에 실행
     @Async
-    @Scheduled(fixedDelay = 60000)  // 1분마다 실행 (60,000 밀리초 = 1분)
+    @Scheduled(cron = "0 0 0 * * *")  // 초, 분, 시, 일, 월, 요일
     public void checkTime() {
-        // 현재 시간을 출력하는 로직
         LocalDateTime now = LocalDateTime.now();
-//        service.changeStatusToTimeOut(now);
 
+        service.changeStatusToTimeOut(now);
 
-//        System.out.println("현재 시간: " + formattedNow + " - 스레드: " + Thread.currentThread().getName());
+        System.out.println("스케줄 실행 - 현재 시간: " + now);
     }
 }
